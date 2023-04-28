2aef225f4a159459a0d6825e53bc49689d465086
Amanda Win <amandawin@Amandas-MacBook-Pro.local>
2023-04-28 15:49:02 -0400

    working json

--- script.js
+++ script.js
@@ -1,44 +1,55 @@
-fetch('path/to/playlist.json')
-  .then(response => response.json())
-  .then(data => {
-   
-    const playlistsData = [
-        {
-          name: 'Blue',
-          color: '#0a52efff',
-          tracks: [
-            {
-              name: 'And We Knew It Was Our Time',
-              url: 'https://open.spotify.com/track/2tIv3DITNBTTayAM6Rewqi?si=77a239f683994b56'
-            },
-            {
-              name: 'An Analog Guy in a Digital World',
-              url: 'https://open.spotify.com/track/1CNvWuZSW8wiTmz6H011xU?si=72f37e22ba5041ab'
-            },
-            {
-              name: 'Lightning Over Heaven',
-              url: 'https://open.spotify.com/track/0S0zgiheqNBkRjEMo7pnig?si=cb7926ebaabe4e65'
-            }
-          ]
-        },
-        {
-          name: 'White',
-          color: '#FFFFFF',
-          tracks: [
-            // Repeat for each playlist
-          ]
-        },
-        // Repeat for each playlist
-      ]
-      
-      function handleClick(event) {
-        const playlistId = event.target.id
-        const playlistData = playlistsData.find(playlist => playlist.name === playlistId)
-        const randomTrack = playlistData.tracks[Math.floor(Math.random() * playlistData.tracks.length)]
-        window.open(randomTrack.url, '_blank')
-      }
-      
-      document.querySelectorAll('.playlist-box').forEach(box => box.addEventListener('click', handleClick))
-      
-  })
-  .catch(error => console.error(error))
+let colorMapping = {
+  "blue": "#0a52efff",
+  "yellow": "#f5e02dff",
+  "green": "#42bb52ff",
+  "purple": "#9b04eaff",
+  "red": "#d01e17ff",
+  "orange": "#fa9b15ff",
+  "white": "#fff",
+  "black": "#000", 
+}
+
+function createBox(data) {
+let color = colorMapping[data.name.toLowerCase()];
+console.log(color);
+
+let container = document.getElementById('container');
+
+//Code to create Box
+
+let createBox = document.createElement('div');
+let box = container.appendChild(createBox);
+
+box.classList.add('playlist-box');
+box.style.background = color;
+
+box.addEventListener('click', function() {
+  let songNumber = randomInt(0, data.tracks.length - 1);
+  window.open(data.tracks[songNumber].url);
+});
+
+}
+
+function randomInt(min, max) {
+return Math.floor(Math.random() * (max - min + 1)) + min;
+/* 
+    Example Use 
+        randomInt(4,10);
+*/
+}
+
+
+fetch('playlist.json')
+.then(response => response.json())
+.then(data => {
+
+  let playlists = data.playlists;
+
+  for (let i = 0; i < playlists.length; i++) {
+    let playlist = playlists[i];
+    createBox(playlist);
+  }
+})
+.catch(error => console.error(error));
+
+
